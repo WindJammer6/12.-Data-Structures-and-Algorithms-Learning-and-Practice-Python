5c9bd3ae95cc65dd6fe429284624b45440f724df
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2026-03-15 17:30:53 +0800

    Delete Tutorial 9.2 - Heap (Binary Tree Data Structure variation) and Max Heap (Heap Data Structure variation) and Min Heap (Heap Data Structure variation) directory

--- Tutorial 9.2 - Heap (Binary Tree Data Structure variation) and Max Heap (Heap Data Structure variation) and Min Heap (Heap Data Structure variation)/2. HeapNode_Class.py
+++
@@ -1,9 +0,0 @@
-# Exactly the same as the 'BinarySearchTreeNode' class in the '2. BinarySearchTreeNode_Class.py' file in the
-# 'Tutorial 9.1 - Binary Trees (Tree Data Structure variation) and Binary Search Trees (Binary Tree Data Structure 
-# variation)' folder
-class HeapNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None  # Left child
-        self.right = None  # Right child
-        self.parent = None  # Parent node
