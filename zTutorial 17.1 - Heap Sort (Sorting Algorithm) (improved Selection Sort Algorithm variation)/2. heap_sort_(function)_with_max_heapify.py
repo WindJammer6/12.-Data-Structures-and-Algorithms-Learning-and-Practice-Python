c2fd8801d8edd0543c036f622723840b33da2d16
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2026-03-17 10:12:14 +0800

    Update 2. heap_sort_(function)_with_max_heapify.py

--- zTutorial 17.1 - Heap Sort (Sorting Algorithm) (improved Selection Sort Algorithm variation)/2. heap_sort_(function)_with_max_heapify.py
+++ zTutorial 17.1 - Heap Sort (Sorting Algorithm) (improved Selection Sort Algorithm variation)/2. heap_sort_(function)_with_max_heapify.py
@@ -26,12 +26,19 @@ def max_heapify(array, heap_size, index):
 def heap_sort_with_max_heapify(array):
     heap_size = len(array)
 
+    # Build max heap
     for index in range(heap_size // 2 - 1, -1, -1):
         max_heapify(array, heap_size, index)
 
+    # One by one extract an element from max heap
     for last_element_index in range(len(array) - 1, 0, -1):
+        # Move current root to end
         array[0], array[last_element_index] = array[last_element_index], array[0]
+
+        # Reduce heap
         heap_size -= 1
+
+        # Call max heapify on the reduced heap
         max_heapify(array, heap_size, 0)
 
 
